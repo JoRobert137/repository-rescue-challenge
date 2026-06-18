24c813f3f6383193d2922074e000aa0874b543a8
JoRobert137 <rjohn.robert@kalvium.community>
2026-06-18 09:56:12 +0530

    Fixed port issue and implemented CORS

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const express = require('express');
+const cors = require('cors');
 const { PORT } = require('./src/config');
 const app = express();
+
+app.use(cors);
 
 app.get('/', (req, res) => {
   res.send(`DevOps assessment app is running on port ${PORT}`);
